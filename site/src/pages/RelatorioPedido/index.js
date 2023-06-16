d5af736286869e0247c6b6bdd95e77dfac4a1715
Giuseppe-Pallazini <Pepepepito1883@gmail.com>
2023-06-16 15:55:02 -0300

    parte grafica 1.0

--- site/src/pages/RelatorioPedido/index.js
+++ site/src/pages/RelatorioPedido/index.js
@@ -4,8 +4,8 @@ import '../../assets/common.scss'
 export default function Index() {
 
     return(
-        <main classname="main-RP">
-            <h1 classname=""> Relatório Pedidos</h1>
+        <main classname='main-RP'>
+            <h1 classname='h1-RP'> Relatório Pedidos</h1>
         </main>
     );
 }
